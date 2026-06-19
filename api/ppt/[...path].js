268b2db07110492a0e584f943402803baff8061d
dark <zyt6724@gmail.com>
2026-06-19 18:20:39 +0800

    Fix Vercel PPT API path proxy

--- api/ppt/[...path].js
+++ api/ppt/[...path].js
@@ -1,6 +1,8 @@
 const { proxy } = require('../_proxy');
 
 module.exports = async function handler(req, res) {
-  const parts = Array.isArray(req.query.path) ? req.query.path : [];
-  return proxy(req, res, '/api/ppt/' + parts.map(encodeURIComponent).join('/'));
+  const rawUrl = req.url || '';
+  const pathname = rawUrl.split('?')[0] || '';
+  const suffix = pathname.replace(/^\/api\/ppt\/?/, '');
+  return proxy(req, res, '/api/ppt/' + suffix);
 };
