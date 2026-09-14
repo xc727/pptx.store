ddc7b0c58e07cb5d63e0ea6d8d50c024b9d9bfe4
xc727 <zyt6724@gmail.com>
2026-09-14 13:29:27 +0800

    Remove conflicting legacy Vercel job routes

--- api/ppt/jobs/[job_id]/preview.js
+++
@@ -1,5 +0,0 @@
-const { proxy } = require('../../../_proxy');
-
-module.exports = async function handler(req, res) {
-  return proxy(req, res, '/api/ppt/jobs/' + encodeURIComponent(req.query.job_id || '') + '/preview');
-};
